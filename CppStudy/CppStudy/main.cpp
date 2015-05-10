ef54e6e25db4f158284e65cb459654bf529e168f
Time1ess <449195172@qq.com>
2015-05-10 22:47:31 +0800

    1

--- CppStudy/CppStudy/main.cpp
+++ CppStudy/CppStudy/main.cpp
@@ -1,18 +1,6 @@
 #include <iostream>
 using namespace std;
 
-int main()
+void main()
 {
-	int n, total = 0;
-	while (cin >> n)
-	{
-		total = n;
-		while (n >= 5)
-		{
-			total += n / 5;
-			n = n / 5 + n % 5;
-		}
-		cout << total << endl;
-	}
-	return 0;
 }
