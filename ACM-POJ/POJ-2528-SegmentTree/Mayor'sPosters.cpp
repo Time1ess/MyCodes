fe12d51bf602e3ea3ca843d9dab7937bd6faaa55
Time1ess <449195172@qq.com>
2015-07-20 18:41:20 +0800

    update

--- ACM-POJ/POJ-2528-SegmentTree/Mayor'sPosters.cpp
+++ ACM-POJ/POJ-2528-SegmentTree/Mayor'sPosters.cpp
@@ -43,9 +43,11 @@ void Update(int l,int r,int v,int cover)
 {
 	if(lines[v].left==l&&lines[v].right==r)
 	{
-		lines[v].kind=cover;
+		lines[v].kind=cover;//如果节点v与插入线段完全重合，只需将覆盖种类更改为cover代表的种类
 		return;
 	}
+	//如果节点v已被覆盖，且其类型！=cover，则先将v内部范围更改成v的类型，然后v本身变成0,
+	//接下来再具体处理l-r的覆盖情况
 	if(lines[v].kind!=0&&lines[v].kind!=cover)
 	{
 		lines[v*2].kind=lines[v].kind;
@@ -105,28 +107,30 @@ int main()
 		for(i=j=1;i<=n;i++)
 		{
 			scanf("%d %d",&l[i],&r[i]);
-			poster[j++].coord=l[i];
+			poster[j++].coord=l[i];	//记录每一幅海报的坐标，部分左右，以便离散化
 			poster[j++].coord=r[i];
 		}
 		qsort(poster+1,n*2,sizeof(item),Cmp);
 		for(i=j=1;i<=2*n;i++,j++)
 		{
 			poster[j].coord=poster[i].coord;
-			poster[j].id=j;
-			while(poster[i].coord==poster[i+1].coord)
+			poster[j].id=j;	//每个坐标绑定一个编号
+			while(poster[i].coord==poster[i+1].coord)	//删除重复的坐标
 				i++;
 		}
 		Build(1,j-1,1);
 		for(i=1;i<=n;i++)
 		{
-			tl.coord=l[i];
+			tl.coord=l[i];	//tl是起始坐标，tr是中点坐标，二者是同一条线段的两个端点
 			tr.coord=r[i];
 			templ=(item *)bsearch(&tl,poster+1,j,sizeof(item),Cmp);
+				//二分找出tl的地址，以求出其编号
 			tempr=(item *)bsearch(&tr,poster+1,j,sizeof(item),Cmp);
-			Update(templ->id,tempr->id,1,i);
+				//二分找出tr的地址，以求出其编号
+			Update(templ->id,tempr->id,1,i);	//插入两个编号构成的线段，更新
 		}
 		result=0;
-		Cal(1);
+		Cal(1);	//统计，根节点是1
 		printf("%d\n",result);
 	}
 	return 0;
