a6585f43a40badd196e48c0c8e33a24bd3819d73
Time1ess <449195172@qq.com>
2015-08-09 15:11:28 +0800

    upload

---
+++ Python/2.2.5 user permission/users.py
@@ -0,0 +1,11 @@
+database=[
+    ['albert','1234'],
+    ['dilbert','4242'],
+    ['smith','7254'],
+    ['jones','9843']
+    ]
+username=raw_input("User name:")
+pin=raw_input("PIN code:")
+
+if [username,pin] in database:print "Access granted"
+else:print "Access denied"
