a796c17645138759cfa7d9cbe205a6b7440badd5
Time1ess <449195172@qq.com>
2015-08-19 15:44:00 +0800

    upload chap 10 template system with re

---
+++ Python/10.3.8 a template system.py
@@ -0,0 +1,34 @@
+import fileinput,re
+
+#use for replace the special text
+#like:
+#'The sum of 7 and 9 is [7+9].'
+#'[name="Mr. Gumby"]Hello, [name]'
+
+
+
+#pattern
+field_pat=re.compile(r'\[(.+?)\]')
+
+#vars collection
+scope={}
+
+#for re.sub
+def replacement(match):
+	code=match.group(1)
+	try:
+		#if able to eval
+		return str(eval(code,scope))
+	except SyntaxError:
+		#otherwise execute assignment in the same scope
+		exec code in scope
+		return ''
+
+#get inputs into a string
+lines=[]
+for line in fileinput.input():
+	lines.append(line)
+text=''.join(lines)
+
+#replace match items using pattern
+print field_pat.sub(replacement,text)
