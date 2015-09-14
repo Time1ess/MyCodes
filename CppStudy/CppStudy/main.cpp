996ff97e637c4b0a5a6bce808ea37d11f89f82a4
Time1ess <449195172@qq.com>
2015-09-14 11:39:20 +0800

    update a test

--- CppStudy/CppStudy/main.cpp
+++ CppStudy/CppStudy/main.cpp
@@ -1,15 +1,74 @@
-/*
-# Author: David - youchen.du@gmail.com
-# QQ : 449195172
-# Last modified:	2015-06-17 20:56
-# Filename:		main.cpp
-# Description: 
-*/
 #include <iostream>
+#include <cstdio>
+#pragma warning(disable:4996)
 using namespace std;
+
+
+
+struct node
+{
+	int val;
+	node *next;
+};
+
+node* create(int num)
+{
+	node *start, *p;
+	start = new node;
+	start->val = 1;
+	start->next = NULL;
+	p = start;
+	for (int i = 2; i <= num; i++)
+	{
+		p->next = new node;
+		p = p->next;
+		p->val = i;
+	}
+	p->next = start;
+	return start;
+}
 
 int main()
 {
-	cout<<"hello world"<<endl;
+	int num;
+	while (scanf("%d", &num) != EOF)
+	{
+		node *head = create(num);
+		node *cur = head->next;
+		node *pri = head;
+		int k = 1;
+		while (cur != pri)
+		{
+			for (; cur!= head;)
+			{
+				k++;
+				if (k == 3)
+				{
+					pri->next = cur->next;
+					delete cur;
+					cur = pri->next;
+					k = 0;
+				}
+				else
+				{
+					cur = cur->next;
+					pri = pri->next;
+				}
+			}
+			if (cur->next == pri&&k == 2)
+			{
+				delete cur;
+				cur = pri;
+				break;
+			}
+			head = cur;
+			pri = head;
+			cur = head->next;
+			k = 1;
+
+		}
+		printf("%d\n", cur->val);
+		delete cur;
+	}
 	return 0;
 }
