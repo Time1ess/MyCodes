4068a8c83252853a541372d7f5e4c7a964d53ebd
Time1ess <449195172@qq.com>
2015-10-01 21:32:25 +0800

    Django update with generic views

--- Django-Projects/mysite/polls/urls.py
+++ Django-Projects/mysite/polls/urls.py
@@ -3,8 +3,12 @@ from django.conf.urls import url
 from . import views
 
 urlpatterns=[
-		url(r'^$',views.index,name='index'),
-		url(r'^(?P<question_id>[0-9]+)/$',views.detail,name='detail'),
-		url(r'^(?P<question_id>[0-9]+)/results/$',views.results,name='results'),
-		url(r'^(?P<question_id>[0-9]+)/vote/$',views.vote,name='vote'),
+		#url(r'^$',views.index,name='index'),
+		url(r'^$',views.IndexView.as_view(),name='index'),
+		#url(r'^(?P<question_id>[0-9]+)/$',views.detail,name='detail'),
+		url(r'^(?P<pk>[0-9]+)/$',views.DetailView.as_view(),name='detail'),
+		#url(r'^(?P<question_id>[0-9]+)/results/$',views.results,name='results'),
+		url(r'^(?P<pk>[0-9]+)/results/$',views.ResultsView.as_view(),name='results'),
+		#url(r'^(?P<question_id>[0-9]+)/vote/$',views.vote,name='vote'),
+		url(r'^(?P<question_id>[0-9]+)vote/$',views.vote,name='vote'),
 		]
