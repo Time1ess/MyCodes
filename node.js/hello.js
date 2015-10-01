55ab8eafc49ac26876a7a295427632aba431be7a
Time1ess <449195172@qq.com>
2015-10-01 11:09:54 +0800

    node js module

--- node.js/hello.js
+++ node.js/hello.js
@@ -1,2 +1,9 @@
 'use strict';
-console.log('Hello world.');
+
+var s="Hello";
+
+function greet(name){
+	console.log(s+', '+name+'!');
+}
+
+module.exports=greet;
---
+++ node.js/main.js
@@ -0,0 +1,7 @@
+'use strict';
+
+var greet=require('./hello');
+
+var s='Michael';
+
+greet(s);
