856b20530101d32154eae51de3de5739e955da9e
Time1ess <449195172@qq.com>
2015-10-02 09:58:39 +0800

    create tests for views

--- Django-Projects/mysite/polls/views.py
+++ Django-Projects/mysite/polls/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render,get_object_or_404
 from django.http import HttpResponse,HttpResponseRedirect
 from django.core.urlresolvers import reverse
 from django.views import generic
+from django.utils import timezone
 
 from .models import Question,Choice
 
@@ -23,12 +24,21 @@ class IndexView(generic.ListView):
 	context_object_name='latest_question_list'
 
 	def get_queryset(self):
-		"""Return the last five published questions."""
-		return Question.objects.order_by('-pub_date')[:5]
+		"""
+		Return the last five published question (not including those set to be 
+		published in the future).
+		"""
+		return Question.objects.filter(pub_date__lte=timezone.now()).order_by('-pub_date')[:5]
 
 class DetailView(generic.DetailView):
 	model=Question
 	template_name='polls/detail.html'
+
+	def get_queryset(self):
+		"""
+		Excludes any questions that aren't published yet.
+		"""
+		return Question.objects.filter(pub_date__lte=timezone.now())
 
 class ResultsView(generic.DetailView):
 	model=Question
