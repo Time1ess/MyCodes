d376528dc11f127eb9257db5bc9a6242e2a7ccc4
Time1ess <449195172@qq.com>
2015-10-02 08:28:33 +0800

    fix a missing slash

--- Django-Projects/mysite/polls/urls.py
+++ Django-Projects/mysite/polls/urls.py
@@ -10,5 +10,5 @@ urlpatterns=[
 		#url(r'^(?P<question_id>[0-9]+)/results/$',views.results,name='results'),
 		url(r'^(?P<pk>[0-9]+)/results/$',views.ResultsView.as_view(),name='results'),
 		#url(r'^(?P<question_id>[0-9]+)/vote/$',views.vote,name='vote'),
-		url(r'^(?P<question_id>[0-9]+)vote/$',views.vote,name='vote'),
+		url(r'^(?P<question_id>[0-9]+)/vote/$',views.vote,name='vote'),
 		]
