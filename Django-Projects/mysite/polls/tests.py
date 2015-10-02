26862228e70aa383ca1cda995abdb2e5665418ae
Time1ess <449195172@qq.com>
2015-10-02 08:59:29 +0800

    create tests for polls app

--- Django-Projects/mysite/polls/tests.py
+++ Django-Projects/mysite/polls/tests.py
@@ -1,3 +1,34 @@
 from django.test import TestCase
+import datetime
+from django.utils import timezone
 
-# Create your tests here.
+from .models import Question
+
+class QuestionMethodTests(TestCase):
+
+	def test_was_published_recently_with_future_question(self):
+		"""
+		was_published_recently() should return False for question whose
+		pub_date is in the future.
+		"""
+		time=timezone.now()+datetime.timedelta(days=30)
+		future_question=Question(pub_date=time)
+		self.assertEqual(future_question.was_published_recently(),False)
+
+	def test_was_published_recently_with_old_question(self):
+		"""
+		was_published_recently() should return False for quesiton whose
+		pub_date is older than 1 day.
+		"""
+		time=timezone.now()-datetime.timedelta(days=30)
+		old_question=Question(pub_date=time)
+		self.assertEqual(old_question.was_published_recently(),False)
+
+	def test_was_published_recently_with_recent_question(self):
+		"""
+		was_published_recently() should return True for question whose
+		pub_date is within the last day.
+		"""
+		time=timezone.now()-datetime.timedelta(hours=1)
+		recent_question=Question(pub_date=time)
+		self.assertEqual(recent_question.was_published_recently(),True)
