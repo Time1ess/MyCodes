630fa54407bee7990a449c0cf5a9b7cef2bad5a7
Time1ess <449195172@qq.com>
2015-10-04 21:40:18 +0800

    fix utf-8 encode error

--- Django-Projects/party/notifications/views.py
+++ Django-Projects/party/notifications/views.py
@@ -14,7 +14,7 @@ def detail(request):
 	notification=get_object_or_404(Notification,pk=notification_id)
 	response_data={}
 	response_data['title']=notification.title
-	response_data['pub_date']=notification.pub_date.strftime('%YƒÍ%m‘¬%d»’ %H:%M:%S')
+	response_data['pub_date']=notification.pub_date.strftime('%Y-%m-%d %H:%M:%S')
 	response_data['content']=notification.content
 	response_data['id']=notification.id
 	additionalFiles=AdditionalDocument.objects.filter(notification_set__id=notification_id)
