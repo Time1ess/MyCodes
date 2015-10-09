7cef80d004f5a5371a99e2faada41ad5d3829967
Time1ess <449195172@qq.com>
2015-10-09 17:05:02 +0800

    sessions

--- Django-Projects/tango_with_django_project/rango/views.py
+++ Django-Projects/tango_with_django_project/rango/views.py
@@ -4,13 +4,37 @@ from rango.models import Category,Page
 from rango.forms import CategoryForm,PageForm,UserForm,UserProfileForm
 from django.contrib.auth import authenticate,login,logout
 from django.contrib.auth.decorators import login_required
+from datetime import datetime
 def index(request):
 	category_list=Category.objects.order_by('-likes')[:5]
 	page_list=Page.objects.order_by('-views')[:5]
 	context_dict={'categories':category_list,
 			'pages':page_list,
 			}
-	return render(request,'rango/index.html',context_dict)
+	visits=request.session.get('visits')
+	if not visits:
+		visits=1
+
+	reset_last_visit_time=False
+	last_visit=request.session.get('last_visit')
+	if last_visit:
+		last_visit_time=datetime.strptime(last_visit[:-7],"%Y-%m-%d %H:%M:%S")
+
+
+		if(datetime.now()-last_visit_time).days>0:
+			visits=visits+1
+			reset_last_visit_time=True
+	else:
+		reset_last_visit_time=True
+
+	
+	if reset_last_visit_time:
+		request.session['last_visit']=str(datetime.now())
+		request.session['visits']=visits
+
+	context_dict['visits']=visits
+	response=render(request,'rango/index.html',context_dict)
+	return response
 
 def about(request):
 	context_dict={'boldmessage':'I am bold font from the context to About',}
