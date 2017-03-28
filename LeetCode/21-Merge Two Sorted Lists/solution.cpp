8123c37e106acb4e3f9bf276bb786fe948872b5f
David <youchen.du@gmail.com>
2017-03-28 10:29:16 +0800

    [add] Leetcode 21.

---
+++ LeetCode/21-Merge Two Sorted Lists/solution.cpp
@@ -0,0 +1,38 @@
+// Author: David
+// Email: youchen.du@gmail.com
+// Created: 2017-03-28 10:29
+// Last modified: 2017-03-28 10:29
+// Filename: solution.cpp
+// Description:
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode* mergeTwoLists(ListNode* l1, ListNode* l2) {
+        ListNode dummy(INT_MIN);
+        ListNode *tail;
+        tail = &dummy;
+        while(l1 && l2)
+        {
+            if(l1->val < l2->val)
+            {
+                tail->next = l1;
+                l1 = l1->next;
+            }
+            else
+            {
+                tail->next = l2;
+                l2 = l2->next;
+            }
+            tail = tail->next;
+        }
+        tail->next = l1 ? l1 : l2;
+        return dummy.next;
+    }
+};
