9e7466855aa7d63da04470ea772958a20607a024
David <youchen.du@gmail.com>
2017-05-03 09:11:49 +0800

    [add] LeetCode 507.

---
+++ LeetCode/357-Count Numbers with Unique Digits/507-Perfect Number/solution.cpp
@@ -0,0 +1,16 @@
+// Author: David
+// Email: youchen.du@gmail.com
+// Created: 2017-05-03 09:11
+// Last modified: 2017-05-03 09:11
+// Filename: solution.cpp
+// Description:
+class Solution {
+public:
+    bool checkPerfectNumber(int num) {
+        int sum = 1;
+        for(int i = 2; i < sqrt(num); i++)
+            if(num % i == 0)
+                sum += i + (num / i == i ? 0 : num / i);
+        return num != 1 and sum == num;
+    }
+};
