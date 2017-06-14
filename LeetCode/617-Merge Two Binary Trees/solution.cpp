10f73ddcbe9d68df516edf772265ae83846df2b6
David <youchen.du@gmail.com>
2017-06-14 19:58:38 +0800

    [add] LeetCode 617.

---
+++ LeetCode/617-Merge Two Binary Trees/solution.cpp
@@ -0,0 +1,30 @@
+// Author: David
+// Email: youchen.du@gmail.com
+// Created: 2017-06-14 19:58
+// Last modified: 2017-06-14 19:58
+// Filename: solution.cpp
+// Description:
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    TreeNode* mergeTrees(TreeNode* t1, TreeNode* t2) {
+        if(t1 == NULL && t2 == NULL)
+            return NULL;
+        if(t1 == NULL)
+            return t2;
+        if(t2 == NULL)
+            return t1;
+        t1->val += t2->val;
+        t1->left = mergeTrees(t1->left, t2->left);
+        t1->right = mergeTrees(t1->right, t2->right);
+        return t1;
+    }
+};
