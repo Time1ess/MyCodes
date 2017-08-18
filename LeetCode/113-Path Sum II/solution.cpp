6d8e7bb82325ace09559f2992e0ad581becf79d7
David <youchen.du@gmail.com>
2017-08-18 08:46:38 +0800

    [update] LeetCode 113.

--- LeetCode/113-Path Sum II/solution.cpp
+++ LeetCode/113-Path Sum II/solution.cpp
@@ -1,7 +1,7 @@
 // Author: David
 // Email: youchen.du@gmail.com
-// Created: 2017-08-18 08:44
-// Last modified: 2017-08-18 08:44
+// Created: 2017-08-18 08:46
+// Last modified: 2017-08-18 08:46
 // Filename: solution.cpp
 // Description:
 /**
@@ -19,14 +19,9 @@ public:
     {
         if(root == NULL)
             return;
-        else if(root->left == NULL && root->right == NULL && root->val == sum)
-        {
-            current.push_back(root->val);
-            ans.push_back(current);
-            current.pop_back();
-            return;
-        }
         current.push_back(root->val);
+        if(root->left == NULL && root->right == NULL && root->val == sum)
+            ans.push_back(current);
         match_sum(ans, current, root->left, sum - root->val);
         match_sum(ans, current, root->right, sum - root->val);
         current.pop_back();
