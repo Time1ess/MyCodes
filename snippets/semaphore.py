9a1a671f33ec87e3c6dc0025525c430b511c83a9
David <youchen.du@gmail.com>
2018-01-18 08:49:32 +0800

    [update] No need for lock.

--- snippets/semaphore.py
+++ snippets/semaphore.py
@@ -1,51 +1,46 @@
 from random import random
 from time import sleep
-from threading import Thread, Semaphore, Lock, get_ident
+from threading import Thread, Semaphore, get_ident
 from itertools import chain
 
 
-N = 4
-M = 2
-def producer(lock, semaphore):
+N = 9
+M = 3
+
+
+def producer(semaphore):
     for _ in range(M):
-        print('Producer {} sleep'.format(get_ident()))
-        sleep(1 + random())
+        sleep(0.5 + random())
         print('Producer {} wakeup'.format(get_ident()))
-        lock.acquire()
         print('Producer {} produce'.format(get_ident()))
         semaphore.release()
-        lock.release()
     print('Producer {} exit'.format(get_ident()))
 
 
-def customer(lock, semaphore):
+def customer(semaphore):
     cnt = 0
     while cnt < N:
-        lock.acquire()
-        product = semaphore.acquire(blocking=False)
-        lock.release()
+        product = semaphore.acquire()
         if product:
             cnt += 1
             print('Customer {} consume'.format(get_ident()))
         else:
             print('No product, Customer {} sleep'.format(get_ident()))
-            sleep(1)
+            # sleep(1)
             print('Customer {} wakeup'.format(get_ident()))
     print('Customer {} exit'.format(get_ident()))
-
 
 
 pts = []
 cts = []
 
 semaphore = Semaphore(0)
-lock = Lock()
 
 for i in range(N // M):
-    pts.append(Thread(target=producer, args=(lock, semaphore)))
+    pts.append(Thread(target=producer, args=(semaphore,)))
 
 for i in range(1):
-    cts.append(Thread(target=customer, args=(lock, semaphore)))
+    cts.append(Thread(target=customer, args=(semaphore,)))
 
 for t in chain(pts, cts):
     t.start()
