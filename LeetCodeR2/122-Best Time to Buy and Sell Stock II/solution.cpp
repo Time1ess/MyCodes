7fe10c62b35ec4652b7a6ca47fee4b302bfa866d
David <youchen.du@gmail.com>
2018-02-09 16:11:24 +0800

    [add] LeetCode 122

---
+++ LeetCodeR2/122-Best Time to Buy and Sell Stock II/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if(n == 0)
+            return 0;
+        vector<vector<int>> dp(n, vector<int>(2, 0));
+        dp[0][0] = 0;
+        dp[0][1] = -prices[0];
+        for(int i = 1; i < n; i++)
+        {
+            dp[i][0] = max(dp[i-1][0], dp[i-1][1] + prices[i]); // Max profit with no stocks at day i
+            dp[i][1] = max(dp[i-1][1], dp[i-1][0] - prices[i]);
+        }
+        return dp[n-1][0];
+    }
+};
