5779cbb6d45a834e78a0dca6f6196e81d92a7122
David <youchen.du@gmail.com>
2018-02-22 16:19:34 +0800

    [add] LeetCode 101

---
+++ LeetCodeR2/101-Symmetric Tree/solution2.cpp
@@ -0,0 +1,35 @@
+/**
+ * Definition for a binary tree node.
+ * struct TreeNode {
+ *     int val;
+ *     TreeNode *left;
+ *     TreeNode *right;
+ *     TreeNode(int x) : val(x), left(NULL), right(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+        if(!root)
+            return true;
+        queue<TreeNode*> q1, q2;
+        q1.push(root->left);
+        q2.push(root->right);
+        while(!q1.empty() && !q2.empty())
+        {
+            TreeNode *left = q1.front();
+            q1.pop();
+            TreeNode *right = q2.front();
+            q2.pop();
+            if(!left && !right)
+                continue;
+            if(!left || !right || left->val != right->val)
+                return false;
+            q1.push(left->left);
+            q1.push(left->right);
+            q2.push(right->right);
+            q2.push(right->left);
+        }
+        return true;
+    }
+};
