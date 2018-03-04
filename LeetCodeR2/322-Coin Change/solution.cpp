f6ce067faf452bb06313a1b80f519ad9dfed53cd
David <youchen.du@gmail.com>
2018-03-04 12:02:58 +0800

    [add] LeetCode 322

---
+++ LeetCodeR2/322-Coin Change/solution.cpp
@@ -0,0 +1,12 @@
+class Solution {
+public:
+    int coinChange(vector<int>& coins, int amount) {
+        vector<int> dp(amount + 1, amount + 1);
+        dp[0] = 0;
+        for(int i = 1; i <= amount; i++)
+            for(auto x: coins)
+                if(i >= x)
+                    dp[i] = min(dp[i], dp[i-x] + 1);
+        return (dp[amount] == amount + 1) ? -1 : dp[amount];
+    }
+};
