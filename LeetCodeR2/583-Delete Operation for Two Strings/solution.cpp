a06032ecf9e7261e4bd572b117a7ca77168c62d0
David <youchen.du@gmail.com>
2018-03-05 11:09:20 +0800

    [add] LeetCode 583

--- LeetCodeR2/583-Delete Operation for Two Strings/solution.cpp
+++ LeetCodeR2/583-Delete Operation for Two Strings/solution.cpp
@@ -4,17 +4,18 @@ public:
         int n = word1.length(), m = word2.length();
         if(n == 0 || m == 0)
             return max(n, m);
-        vector<vector<int>> dp(n + 1, vector<int>(m + 1, 0));  // dp[i][j]: minimum deletions for s1 of len i and s2 of len j
-        for(int i = 1; i <= n; i++)
-            dp[i][0] = i;
+        vector<vector<int>> dp(2, vector<int>(m + 1, 0));  // dp[i][j]: minimum deletions for s1 of len i and s2 of len j
         for(int j = 1; j <= m; j++)
             dp[0][j] = j;
         for(int i = 1; i <= n; i++)
+        {
+            dp[i%2][0] = i;
             for(int j = 1; j <= m; j++)
                 if(word1[i-1] == word2[j-1])
-                    dp[i][j] = dp[i-1][j-1];
+                    dp[i%2][j] = dp[(i+1)%2][j-1];
                 else
-                    dp[i][j] = min(dp[i-1][j] + 1, min(dp[i][j-1] + 1, dp[i-1][j-1] + 2));
-        return dp[n][m];
+                    dp[i%2][j] = min(dp[(i+1)%2][j] + 1, dp[i%2][j-1] + 1);
+        }
+        return dp[n%2][m];
     }
 };
