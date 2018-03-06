5bbf0d8e1d14793486fec11c796daad04f0f0c3a
David <youchen.du@gmail.com>
2018-03-06 10:07:26 +0800

    [add] LeetCode 516

---
+++ LeetCodeR2/516-Longest Palindromic Subsequence/solution.cpp
@@ -0,0 +1,19 @@
+class Solution {
+public:
+    int longestPalindromeSubseq(string s) {
+        int n = s.length();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for(int i = 0; i < n; i++)
+            dp[i][i] = 1;
+        for(int len = 2; len <= n; len++)
+            for(int i = 0; i <= n - len; i++)
+            {
+                int j = i + len - 1;
+                if(s[i] == s[j])
+                    dp[i][j] = dp[i+1][j-1] + 2;
+                else
+                    dp[i][j] = max(dp[i+1][j], dp[i][j-1]);
+            }
+        return dp[0][n-1];
+    }
+};
