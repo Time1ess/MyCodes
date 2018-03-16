7817656816e829c753f54141b3b7e7bed2e28879
David <youchen.du@gmail.com>
2018-03-16 15:34:34 +0800

    [add] LintCode 84

---
+++ LintCode/84-Single Number III/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    /*
+     * @param A: An integer array
+     * @return: An integer array
+     */
+    vector<int> singleNumberIII(vector<int> &A) {
+        // write your code here
+        int x = 0;
+        for(auto k: A)
+            x ^= k;
+        while(x&(x-1))
+            x = x&(x-1);
+        int first = 0, second = 0;
+        for(auto k: A)
+            if(x & k)
+                first ^= k;
+            else
+                second ^= k;
+        return {first, second};
+    }
+};
