df9489b306578edba4d05999fb040d6884911e96
David <youchen.du@gmail.com>
2018-03-19 09:16:15 +0800

    [add] LintCode 718

--- LintCode/718-Repeat String/solution.cpp
+++ LintCode/718-Repeat String/solution.cpp
@@ -8,20 +8,20 @@ public:
     int repeatedString(string &A, string &B) {
         // write your code here
         int len1 = A.length(), len2 = B.length();
-        for(int i = 0; i < len1; i++) // try match from A[i]
+        for(int i = 0; i < len1; i++)
         {
-            bool match = true;
-            int cnt = i == 0 ? 0 : 1;
             int k = i;
+            bool match = true;
+            int cnt = 0;
             for(int j = 0; j < len2; j++)
             {
+                if(j == 0 || (j != 0 && k == 0))
+                    cnt++;
                 if(A[k] != B[j])
                 {
                     match = false;
                     break;
                 }
-                if(k == 0)
-                    cnt++;
                 k = (k + 1) % len1;
             }
             if(match)
