9a5fc19d1766afc5cb4fe8f86bfa8d628f3282d2
David <youchen.du@gmail.com>
2018-03-24 15:11:22 +0800

    [add] LintCode 97

---
+++ LintCode/97-Maximum Depth of Binary Tree/solution.cpp
@@ -0,0 +1,26 @@
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    /**
+     * @param root: The root of binary tree.
+     * @return: An integer
+     */
+    int maxDepth(TreeNode * root) {
+        // write your code here
+        if(!root)
+            return 0;
+        return 1 + max(maxDepth(root->left), maxDepth(root->right));
+    }
+};
