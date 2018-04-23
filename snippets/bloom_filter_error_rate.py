e64751e9a00b3b3368691e33b68bebc235319d84
David <youchen.du@gmail.com>
2018-04-23 15:50:01 +0800

    [update] Plot for bloom filter error rates

--- snippets/bloom_filter_error_rate.py
+++ snippets/bloom_filter_error_rate.py
@@ -1,8 +1,9 @@
-from math import e
+import numpy as np
+import matplotlib.pyplot as plt
 
 
 def bloom_err(m_div_n, k):
-    return (1 - e ** (- k / m_div_n)) ** k
+    return (1 - np.e ** (- k / m_div_n)) ** k
 
 
 K = 12
@@ -10,9 +11,18 @@ M_div_N = 32
 print('m/n\t', end='')
 for k in range(1, K + 1):
     print("K = %d\t\t" % k, end='')
+plt.figure(figsize=(16, 16))
 print()
 for m_div_n in range(2, M_div_N + 1):
     print('%d\t' % m_div_n, end='')
-    for k in range(1, K + 1):
-        print('%.7f\t' % bloom_err(m_div_n, k), end='')
+    errors = [100 * bloom_err(m_div_n, k) for k in range(1, K + 1)]
+    for e in errors:
+        print('%.7f\t' % e, end='')
+    plt.plot(range(1, 1 + K), errors, label='%d' % m_div_n)
     print()
+plt.ylim([0, 100])
+plt.legend(title='M / N', bbox_to_anchor=(1.05, 1), loc=2, borderaxespad=0.)
+plt.ylabel('Error rate(%)')
+plt.xlabel('Number of hash functions')
+plt.title('Bloom filter error rates')
+plt.show()
