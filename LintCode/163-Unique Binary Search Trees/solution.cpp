92d7bd27ccb2025a5838b51c72b2f2b92816f55c
David <youchen.du@gmail.com>
2018-05-11 14:30:46 +0800

    [add] LintCode 163

---
+++ LintCode/163-Unique Binary Search Trees/solution.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    /**
+     * @param n: An integer
+     * @return: An integer
+     */
+    int numTrees(int n) {
+        // write your code here
+        if(n <= 1)
+            return 1;
+        vector<int> dp(n + 1, 0);
+        dp[0] = dp[1] = 1;
+        for(int i = 2; i <= n; i++)
+            for(int j = 0; j < i; j++)
+                dp[i] += dp[j] * dp[i - 1 - j];
+        return dp[n];
+    }
+};
