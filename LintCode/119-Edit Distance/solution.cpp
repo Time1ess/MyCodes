3c955445dfc7f64be0693041b39878f89cfab550
David <youchen.du@gmail.com>
2018-05-26 20:58:23 +0800

    [add] LintCode 119

---
+++ LintCode/119-Edit Distance/solution.cpp
@@ -0,0 +1,26 @@
+class Solution {
+public:
+    /**
+     * @param word1: A string
+     * @param word2: A string
+     * @return: The minimum number of steps.
+     */
+    int minDistance(string &word1, string &word2) {
+        // write your code here
+        int m = word1.length();
+        int n = word2.length();
+        vector<vector<int>> dp(m + 1, vector<int>(n + 1, 0));
+        for(int i = 1; i <= m; i++)
+            dp[i][0] = i;
+        for(int j = 1; j <= n; j++)
+            dp[0][j] = j;
+        for(int i = 1; i <= m; i++)
+            for(int j = 1; j <= n; j++) {
+                if(word1[i-1] == word2[j-1])
+                    dp[i][j] = dp[i-1][j-1];
+                else
+                    dp[i][j] = min(dp[i-1][j-1], min(dp[i][j-1], dp[i-1][j])) + 1;
+            }
+        return dp[m][n];
+    }
+};
