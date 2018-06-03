d5161d69720a54c91e192d69ffada39c7401d5b3
David <youchen.du@gmail.com>
2018-06-03 14:47:32 +0800

    [add] go examples

---
+++ Go/GobyExample/time_fmt_parse.go
@@ -0,0 +1,32 @@
+package main
+
+import "fmt"
+import "time"
+
+func main() {
+  p := fmt.Println
+
+  t := time.Now()
+  p(t.Format(time.RFC3339))
+
+  t1, _ := time.Parse(
+    time.RFC3339,
+    "2012-11-01T22:08:41+00:00")
+  p(t1)
+
+  p(t.Format("3:04PM"))
+  p(t.Format("Mon Jan _2 15:04:05 2006"))
+  form := "3 04 PM"
+  t2, _ := time.Parse(form, "8 41 PM")
+  p(t2)
+
+  fmt.Printf(
+    "%d-%02d-%02dT%02d:%02d:%02d-00:00\n",
+    t.Year(), t.Month(), t.Day(),
+    t.Hour(), t.Minute(), t.Second)
+
+  ansic := "Mon Jan _2 15:04:05 2006"
+  _, e := time.Parse(ansic, "8:41PM")
+  p(e)
+
+}
