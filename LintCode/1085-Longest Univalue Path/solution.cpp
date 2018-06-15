8625ec9c59b61c3bdab409d5d39260caaf43e5b9
David <youchen.du@gmail.com>
2018-06-15 21:15:55 +0800

    [add] LintCode 1085

---
+++ LintCode/1085-Longest Univalue Path/solution.cpp
@@ -0,0 +1,38 @@
+/**
+ * Definition of TreeNode:
+ * class TreeNode {
+ * public:
+ *     int val;
+ *     TreeNode *left, *right;
+ *     TreeNode(int val) {
+ *         this->val = val;
+ *         this->left = this->right = NULL;
+ *     }
+ * }
+ */
+
+class Solution {
+public:
+    /**
+     * @param root: 
+     * @return: the length of the longest path where each node in the path has the same value
+     */
+    int longestUnivalueNodes(TreeNode* root, int& ans, int val) {
+        if(!root)
+            return 0;
+        int left = longestUnivalueNodes(root->left, ans, root->val);
+        int right = longestUnivalueNodes(root->right, ans, root->val);
+        ans = max(ans, left + right);
+        if(root->val == val)
+            return max(left, right) + 1;
+        return 0;
+    }
+    int longestUnivaluePath(TreeNode * root) {
+        if(!root)
+            return 0;
+        int ans = 0;
+        int left = longestUnivalueNodes(root->left, ans, root->val);
+        int right = longestUnivalueNodes(root->right, ans, root->val);
+        return max(ans, left + right);
+    }
+};
