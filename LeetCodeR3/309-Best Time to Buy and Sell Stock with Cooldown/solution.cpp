55fdd99ade9157147243f66f677033a18b812039
David <youchen.du@gmail.com>
2018-08-08 21:40:29 +0800

    [add] LeetCode 309

---
+++ LeetCodeR3/309-Best Time to Buy and Sell Stock with Cooldown/solution.cpp
@@ -0,0 +1,20 @@
+class Solution {
+public:
+    int maxProfit(vector<int>& prices) {
+        int n = prices.size();
+        if(n <= 1) {
+            return 0;
+        } else if(n == 2) {
+            return max(0, prices[1] - prices[0]);
+        }
+        vector<int> buy(n, 0), sell(n, 0);
+        buy[0] = -prices[0];
+        buy[1] = max(-prices[0], -prices[1]);
+        sell[1] = max(0, prices[1] - prices[0]);
+        for(int i = 2; i < n; i++) {
+            buy[i] = max(buy[i-1], sell[i-2] - prices[i]);
+            sell[i] = max(sell[i-1], buy[i-1] + prices[i]);
+        }
+        return sell[n-1];
+    }
+};
