cefcc28adca5899105e9e53429162e5106416f7f
David <youchen.du@gmail.com>
2018-08-11 10:30:29 +0800

    [add] LeetCode 240

---
+++ LeetCodeR3/240-Search a 2D Matrix II/solution.cpp
@@ -0,0 +1,24 @@
+class Solution {
+public:
+    bool searchMatrix(vector<vector<int>>& matrix, int target) {
+        int m = matrix.size();
+        if (m == 0) {
+            return false;
+        }
+        int n = matrix[0].size();
+        if (n == 0) {
+            return false;
+        }
+        int i = 0, j = n - 1;
+        while(i < m && j >= 0) {
+            if (matrix[i][j] == target) {
+                return true;
+            } else if (matrix[i][j] < target) {
+                i++;
+            } else {
+                j--;
+            }
+        }
+        return false;
+    }
+};
