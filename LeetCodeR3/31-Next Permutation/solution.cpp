4338afb0a1028ed0a8b5a3dfe9b0c90e8947b6b2
David <youchen.du@gmail.com>
2018-08-19 14:47:36 +0800

    [add] LeetCode 31

---
+++ LeetCodeR3/31-Next Permutation/solution.cpp
@@ -0,0 +1,22 @@
+class Solution {
+public:
+    void nextPermutation(vector<int>& nums) {
+        int n = nums.size();
+        int idx = n - 2;
+        while (idx >= 0 && nums[idx] >= nums[idx+1]) {
+            idx--;
+        }
+        if (idx == -1) {
+            sort(nums.begin(), nums.end());
+            return;
+        }
+        int swap_idx = idx + 1;
+        for (int i = idx + 1; i < n; i++) {
+            if (nums[idx] < nums[i] && nums[i] < nums[swap_idx]) {
+                swap_idx = i;
+            }
+        }
+        swap(nums[idx], nums[swap_idx]);
+        sort(nums.begin() + idx + 1, nums.end());
+    }
+};
