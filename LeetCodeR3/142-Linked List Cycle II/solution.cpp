f4ed4cc02c442e83f58808657aa395588c992384
David <youchen.du@gmail.com>
2018-08-19 10:56:16 +0800

    [add] LeetCode 142

---
+++ LeetCodeR3/142-Linked List Cycle II/solution.cpp
@@ -0,0 +1,30 @@
+/**
+ * Definition for singly-linked list.
+ * struct ListNode {
+ *     int val;
+ *     ListNode *next;
+ *     ListNode(int x) : val(x), next(NULL) {}
+ * };
+ */
+class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+        if (head == nullptr) {
+            return nullptr;
+        }
+        ListNode* slow = head;
+        ListNode* fast = head;
+        while (fast->next != nullptr && fast->next->next != nullptr) {
+            slow = slow->next;
+            fast = fast->next->next;
+            if (slow == fast) {
+                while (head != slow) {
+                    head = head->next;
+                    slow = slow->next;
+                }
+                return head;
+            }
+        }
+        return nullptr;
+    }
+};
