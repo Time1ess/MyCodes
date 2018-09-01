3ff7eac7d65d31deae63971df360410823ac8b43
David <youchen.du@gmail.com>
2018-09-01 10:05:50 +0800

    [add] LeetCode 877

---
+++ LeetCodeR3/877-Stone Game/solution.cpp
@@ -0,0 +1,17 @@
+class Solution {
+public:
+    bool stoneGame(vector<int>& piles) {
+        int n = piles.size();
+        vector<vector<int>> dp(n, vector<int>(n, 0));
+        for (int i = 0; i < n; i++) {
+            dp[i][i] = piles[i];
+        }
+        for (int len = 2; len <= n; len++) {
+            for (int i = 0; i + len <= n; i++) {
+                int j = i + len - 1;
+                dp[i][j] = max(piles[i] - dp[i+1][j], piles[j] - dp[i][j-1]);
+            }
+        }
+        return dp[0][n-1] > 0;
+    }
+};
